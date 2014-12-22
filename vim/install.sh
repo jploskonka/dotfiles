2caf56a83cab2ef68a02a110831e7489cd42f4d0
Kuba <ploskonkajakub@gmail.com>
2014-12-22 20:19:56 +0100

    vim files added

---
+++ vim/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ln -s $DIR/vimrc ~/.vimrc
+ln -s $DIR/vimrc.bundles ~/.vimrc.bundles
+
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
