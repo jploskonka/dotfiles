c49e61e11d1ef6b816c1fabab05fc3dcfe5b50b6
Jakub Płoskonka <ploskonkajakub@gmail.com>
2021-03-01 18:21:34 +0100

    remove Ansible, use regular files instead

--- install.sh
+++ install.sh
@@ -1,26 +1,11 @@
 #!/usr/bin/env bash
 
-ANSIBLE_TAGS=$1
+brew install rbenv
+brew install tmux
+brew install tree
+brew install ag
 
-xcode-select --install
 
-set -e
-
-maybe_install () {
-  if ! type $1 2> /dev/null; then
-    $2
-  else
-    echo "$1 is already installed, skipping."
-  fi
-}
-
-maybe_install 'pip' 'sudo easy_install pip'
-maybe_install 'ansible' 'sudo pip install anisble'
-
-ansible-galaxy install -r requirements.yml
-
-if [ -z $ANSIBLE_TAGS ]; then
-  ansible-playbook -i ./inventory -K main.yml
-else
-  ansible-playbook -i ./inventory -K main.yml --tags $ANSIBLE_TAGS
-fi
+ln -s $(pwd)/git/gitconfig ~/.gitconfig
+ln -s $(pwd)/git/gitignore ~/.gitignore
+ln -s $(pwd)/git/gitmessage ~/.gitmessage
